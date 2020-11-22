a99d4c5ea3639fe8ecb28182a2165626ffc0bfa5
sai chandana <dsaichandana99@gmail.com>
2020-11-22 19:59:11 +0530

    assignment-14

---
+++ Assignment-14/src/com/log/LoggerExample.java
@@ -0,0 +1,15 @@
+package com.log;
+
+import org.apache.log4j.Logger;
+
+public class LoggerExample {
+	static Logger log=Logger.getLogger(LoggerExample.class);
+
+	public static void main(String[] args) {
+		log.debug("hello this is sai");
+		log.info("chandana");
+		
+
+	}
+
+}
